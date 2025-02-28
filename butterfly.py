170c2b4bce2add8defcd20ff5a606821b018bc17
timo-do <timodo@live.de>
2025-02-28 22:17:34 +0100

    add configuration management and logging to butterfly.py; create quick and printed YAML config files

--- butterfly.py
+++ butterfly.py
@@ -3,24 +3,69 @@ import bases
 import geometryhelpers
 import maphelpers
 import imagehelpers
-from matplotlib import pyplot as plt
+
+import yaml
+import os
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+if("RUNCONFIG" in os.environ):
+    logging.debug(f"Found RUNCONFIG in environment: {os.environ['RUNCONFIG']}")
+    configPath = f'configs/{os.environ["RUNCONFIG"]}.yaml'
+else:
+    logging.info("No RUNCONFIG found in environment, using default (quick) config")
+    configPath = 'configs/quick.yaml'
+logging.info(f"Using config: {configPath}")
+with open(configPath, 'r') as file:
+    config = yaml.safe_load(file)
 
 
-s = 0.8
-a = 0.24
-PP1Leaf = 8000
-PP1Layout = 800
-alpha = np.deg2rad(206)
-beta = np.deg2rad(45)
-gamma = np.deg2rad(0)
+params = config['parameters']
+s = params['s']
+a = params['a']
+PP1Leaf = int(params['PP1']['Leafs'])
+PP1Layout = int(params['PP1']['Layout'])
+alpha = np.deg2rad(params['rotation']['alpha'])
+beta = np.deg2rad(params['rotation']['beta'])
+gamma = np.deg2rad(params['rotation']['gamma'])
+basemap_path = str(params['Basemap'])
+
+logging.info(f"Parameters: s = {s}, a = {a}, PP1Leaf = {PP1Leaf}, PP1Layout = {PP1Layout}, alpha = {alpha}, beta = {beta}, gamma = {gamma}, basemap = {basemap_path}")
 
 yaw, pitch, roll = geometryhelpers.getRotationMatrices(alpha, beta, gamma)
 
-# Base Octahedron (pitched)
+logging.debug("Rotation matrix created")
+
+
 octahedron = bases.createOctahedron() @ pitch
+logging.debug("Base octahedron created, with rotation applied")
+# In the base octahedron, the first two vertices are upper and lower tip (if you hold it like a diamond)
+# We call the inner square the "base". See the function for details.
 tips = octahedron[:2, :]
 base = octahedron[2:, :]
 
+# So one level is one slice of the octahedron (sliced horizontally)
+# This is how we group the vertices. It will make sense when we draw the faces.
+# We have 7 levels: 2 tips
+#          * <- Level 0
+#         / \
+#        /   \  <- Level 1
+#       /     \ 
+#      /       \
+#     /         \
+#    /           \
+#   /             \ <- Level 2
+#  /               \
+# *-----------------* <- Level 3
+#  \               /
+#   \             / <- Level -3
+#    \           /
+#     \         /
+#      \       /
+#       \     /
+#        \   /  <- Level -2
+#         \ /
+#          * <- Level -1
 
 levels = np.zeros(7, dtype = np.ndarray)
 for idxLevel in [0, -1]:
@@ -31,7 +76,7 @@ for idxLevel in [1, 2, -3, -2]:
     for idxCorner in range(4):
         start = tips[int(idxLevel < 0)]
         end = base[idxCorner, :]
-        if(idxLevel in (1, -2)):    
+        if(idxLevel in (1, -2)):
             frac = a
         else:
             frac = (1 - a)
@@ -47,8 +92,6 @@ for leftSide in range(4):
 
 vertices = np.vstack([level for level in levels]) @ yaw @ roll
 
-
-
 def level2idx(level, num = 0):
     vertexCount = [1, 4, 4, 12, 4, 4, 1]
     idx = num
@@ -56,9 +99,12 @@ def level2idx(level, num = 0):
         idx += count
     return idx
 
+# We name each face of the octahedron a leaf, because that's what it looks like in the end
+# We have 8 leaves, each with 7 faces (which are triangles)
 leaves = np.zeros((8, 7, 3), dtype=np.uint8)
 mapFaces = np.zeros((8, 7), dtype=maphelpers.Face)
 for leaf in range(8):
+    # The first 4 leaves are the "upper" faces, the last 4 are the "lower" faces
     if(leaf < 4):
         tip = 0
         upper = 1
@@ -107,9 +153,16 @@ for leaf in range(8):
     leaves[leaf, 6, 1] = level2idx(3, rightLev3)
     leaves[leaf, 6, 2] = level2idx(lower, rightSide)
 
-#bases.plot_solid(vertices, leaves.reshape(-1, 3))
-
-basemap = imagehelpers.load_image("images/blue_marble_august_small.png")
+bases.plot_solid(vertices, leaves.reshape(-1, 3))
+# check if basemap contains a $ character
+if("$" in basemap_path):
+    def getBasemapFiles(col, row):
+        colNames = ["A", "B", "C", "D"]
+        rowNames = ["1", "2"]
+        return basemap_path.replace("$ROW", rowNames[row]).replace("$COL", colNames[col])
+    basemap = imagehelpers.load_basemap(getBasemapFiles)
+else:
+    basemap = imagehelpers.load_image(basemap_path)
 mapFaces = np.zeros((8, 7), dtype=maphelpers.Face)
 
 # alignment charts for each leaf
@@ -125,14 +178,12 @@ for idxLeaf in np.arange(8):
         mapFaces[idxLeaf, idxFace] = maphelpers.Face(face, vertices[face])
 
 # get basemap
-def getBasemapFiles(col, row):
-    colNames = ["A", "B", "C", "D"]
-    rowNames = ["1", "2"]
-    return f"images/base/bathyOcean/world.topo.bathy.200408.3x21600x21600.{colNames[col]}{rowNames[row]}.png"
-    #return f"images/base/test/{colNames[col]}{rowNames[row]}.png"
+
+
+
 
 #basemap = np.array([[[255, 255, 255, 255]]])
-basemap = imagehelpers.load_basemap(getBasemapFiles)
+#basemap = imagehelpers.load_basemap(getBasemapFiles)
 
 def isCommonEdge(mainNodes, neighborNodes):
     return len(np.intersect1d(mainNodes, neighborNodes)) > 1
