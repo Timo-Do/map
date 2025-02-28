4cb65c283bbf1981b32fba41fcd6ca09ba8bb521
timo-do <timodo@live.de>
2025-02-28 22:17:15 +0100

    update plot_solid function to use colormap and adjust transparency

--- bases.py
+++ bases.py
@@ -19,7 +19,7 @@ def plot_solid(vertices, faces):
     z = vertices[:, 2]
 
     ax.plot_trisurf(x, y, z, triangles=faces,
-                        color='viridis', alpha=1, edgecolor='k')
+                        cmap='viridis', alpha=0.2, edgecolor='k')
     ax.set_box_aspect([np.ptp(i) for i in [x, y, z]])
     plt.show()
 
